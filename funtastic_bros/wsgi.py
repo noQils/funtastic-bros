f85c6c0c8e3e216c0c8ccf25a5cc563da87a2412
rafifsulaiman <rafifsulaimann@gmail.com>
2025-07-24 22:14:08 +0700

    startproject

---
+++ funtastic_bros/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for funtastic_bros project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'funtastic_bros.settings')
+
+application = get_wsgi_application()
